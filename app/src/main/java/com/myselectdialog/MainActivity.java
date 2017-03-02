07fe719364db6d5ffe867b4950bf90808fac19f1
songyongmeng <songyongmeng@58ganjil.com>
2017-03-02 21:31:07 +0800

    Initial commit

---
+++ app/src/main/java/com/myselectdialog/MainActivity.java
@@ -0,0 +1,41 @@
+package com.myselectdialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    public MainActivity context;
+    Toast toast;
+    private String[] otherLables = new String[]{"test1","test2"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = this;
+        toast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
+        (findViewById(R.id.show_dialog)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show();
+            }
+        });
+    }
+
+    private void show() {
+        NormalActionSheet as = new NormalActionSheet(this);
+        as.builder().setTitle("title").setItems(Arrays.asList(otherLables)).
+                setListener(new NormalActionSheet.OnNormalItemClickListener() {
+                    @Override
+                    public void onClick(String value) {
+                            toast.setText(value);
+                            toast.show();
+                    }
+                }).show();
+    }
+}
